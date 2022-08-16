391db71d1393e5fbcef27ba592ac53739cdf8397
kiran kuyate <kiranrkuyate2018@gmail.com>
2022-08-16 12:29:42 +0530

    TMDB api is added🔥

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import HomeLayout from "./layouts/Home.my.layout";
 import Movie from "./components/pages/Movie.page";
@@ -8,6 +9,11 @@ import PlayLayout from "./layouts/Plays.layout";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+//axios default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 export default function App() {
   return (
--- src/components/HeroCarousal/HeroCarousal.component.jsx
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSlider from "react-slick";
+import axios from "axios";
 
 import { NextArrow, PrevArrow } from "./Arrows.component";
 
 const HeroCarousal = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const requestNowPlayingMovies = async () => {
+      const getImages = await axios.get("/movie/now_playing");
+      setImages(getImages.data.results);
+    };
+    requestNowPlayingMovies();
+  }, []);
+
   const settingLG = {
     arrow: true,
     autoplay: true,
@@ -27,20 +38,7 @@ const HeroCarousal = () => {
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
   };
-  const images = [
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/timepass-3-et00330615-1658992324.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/ek-villain-returns-et00128946-1658744061.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/vikrant-rona-et00128225-1657019014.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/shamshera-et00075253-1657356503.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/rocketry-et00094131-1655809015.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/thor-love-and-thunder-et00302403-24-05-2022-12-00-43.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/minions-the-rise-of-gru-et00122133-21-04-2022-06-44-55.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/hit-the-first-case-et00321702-17-06-2022-10-39-38.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/ramarao-on-duty-et00312747-1657525661.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/sarsenapati-hambirrao-et00104574-16-05-2022-01-56-11.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/mahaveeryar-et00332365-1656327128.jpg",
-    "https://assets-in.bmscdn.com/iedb/movies/images/extra/horizontal_no_logo/mobile/listing/xxlarge/green-lantern-beware-my-power-et00334223-1658934348.jpg",
-  ];
+
   return (
     <>
       <div className=" lg:hidden">
@@ -48,7 +46,7 @@ const HeroCarousal = () => {
           {images.map((image) => (
             <div className="w-full h-56 md:h-80 py-2 px-1">
               <img
-                src={image}
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                 alt="testing"
                 className="w-full  rounded-md h-full"
               />
@@ -61,7 +59,7 @@ const HeroCarousal = () => {
           {images.map((image) => (
             <div className=" w-full h-96 px-1 py-3  ">
               <img
-                src={image}
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                 alt="testing"
                 className="w-full  rounded-md h-full"
               />
